fd01db484dc6a3522a87c9a52847a997a40cb2ba
Anit Shrestha Manandhar <shrestha.anit@gmail.com>
2015-02-26 12:42:50 +0545

    Added Controller to test BDO jar integration

---
+++ src/LibraryBundle/Controller/BdoController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LibraryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class BdoController extends Controller
+{
+    /**
+     * @Route("/bdo/", name="bdo")
+     * @Template()
+     */
+    public function bdoAction(Request $request)
+    {
+        // if ($request->request->has('username') && $request->request->has('password')) {
+        //     try {
+        //         $username = $request->request->get('username');
+        //         $password = $request->request->get('password');
+        //         $DB = $this->get('connection');
+        //         $result = $DB->getUser(array($username, $password));
+        //         if ($result>1) {
+        //             $session = new Session();
+        //             $session->start();
+
+        //             // set and get session attributes
+        //             $session->set('username', $username);
+
+        //             return $this->redirect($this->generateUrl('dashboard'));
+        //         } else {
+        //             return array('error_msg' => 'Incorrect Username or Password','username' => $username,'password' => $password);
+        //         }
+        //     } catch (\Exception $e) {
+        //         $e->getMessage();
+        //     }
+        // }
+
+        echo 2222;
+        die;
+    }
+}
