e902901ae1c6a0f24ce3e9310dd4192b9c1428b8
krossroad <krossroad07@gmail.com>
2015-04-09 17:13:24 +0545

    Add Log repo

---
+++ src/BackendBundle/Entity/Repository/TransactionLogRepository.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rikesh
+ * Date: 4/9/15
+ * Time: 4:16 PM
+ */
+
+namespace BackendBundle\Entity\Repository;
+
+
+use BackendBundle\Entity\Log;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class TransactonLogRepository
+ * @package BackendBundle\Entity\Repository
+ */
+class TransactionLogRepository extends EntityRepository
+{
+    /**
+     * @param $serviceId
+     * @param $method
+     * @param string $request
+     * @param string $response
+     * @param string $status
+     * @return bool
+     */
+    public function addLog($serviceId, $method, $request = '', $response = '', $status = 'UNKNOWN')
+    {
+        $flag = true;
+        $log = new Log();
+
+        $log->setMethod($method);
+        $log->setService($serviceId);
+        $log->setRequest($request);
+        $log->setResponse($response);
+        $log->setStatus($status);
+
+        try {
+            $this->getEntityManager()->persist($log);
+            $this->getEntityManager()->flush();
+            $this->getEntityManager()->clear();
+
+            $flag = true;
+
+        } catch(\Exception $e) {
+        }
+
+        return $flag;
+    }
+}
