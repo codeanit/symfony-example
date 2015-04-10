9f8fe0dddabf0b52e55bfa6344f8759be99b4bfa
krossroad <krossroad07@gmail.com>
2015-04-10 17:34:03 +0545

    Add test for transaction request

---
+++ src/BackendBundle/Tests/QueueWorkerTest/General/TransactionRequest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rikesh
+ * Date: 4/10/15
+ * Time: 2:30 PM
+ */
+
+namespace BackendBundle\Tests\QueueWorkerTest\General;
+
+
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
+
+class Request extends \PHPUnit_Framework_TestCase
+{
+    public function testRequestSending()
+    {
+
+    }
+} 
