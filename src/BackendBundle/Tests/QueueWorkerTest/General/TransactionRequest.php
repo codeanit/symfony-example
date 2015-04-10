6da08e9569a708d9c3726258055f75af3354877a
krossroad <krossroad07@gmail.com>
2015-04-10 17:42:04 +0545

    ~

--- src/BackendBundle/Tests/QueueWorkerTest/General/TransactionRequest.php
+++ src/BackendBundle/Tests/QueueWorkerTest/General/TransactionRequest.php
@@ -9,12 +9,39 @@
 namespace BackendBundle\Tests\QueueWorkerTest\General;
 
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
 
-class Request extends \PHPUnit_Framework_TestCase
+class TransactionRequest extends WebTestCase
 {
-    public function testRequestSending()
-    {
+    const TRANSACTION_URL = '127.0.0.0/work/cdex-backed/app_dev/php/transactions/v2/creates';
 
+    /**
+     * @dataProvider createHttpClient
+     */
+    public function testTransactionCreate(Client $httpClient)
+    {
+        $request = $httpClient->createRequest('POST', self::TRANSACTION_URL, [
+            'headers' => [
+                'content-type' => 'application/json'
+            ],
+            'body' => [
+
+            ]
+        ]);
+
+        $response = $request->send();
+
+
+
+    }
+
+    public function createHttpClient()
+    {
+        $client = new Client();
+
+        return [
+            [$client]
+        ];
     }
 } 
