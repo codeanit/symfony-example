6ca3181dd8e9fde37029c35f55873a7ad3a65228
krossroad <krossroad07@gmail.com>
2015-04-21 12:57:22 +0545

    Use Qb to fetch queues

--- src/BackendBundle/Entity/Repository/OperationQueueRepository.php
+++ src/BackendBundle/Entity/Repository/OperationQueueRepository.php
@@ -1,14 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rikesh
- * Date: 4/21/15
- * Time: 12:30 PM
- */
 
 namespace BackendBundle\Entity\Repository;
 
 
-class OperationQueueRepository {
+use BackendBundle\Entity\OperationsQueue;
+use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class OperationQueueRepository
+ * @package BackendBundle\Entity\Repository
+ */
+class OperationQueueRepository extends EntityRepository {
+
+    /**
+     * @param array $filters
+     * @param array $orderBy
+     * @param null $perPage
+     * @param null $limit
+     * @return mixed
+     */
+    public function getOperationQueues(array $filters = [], $orderBy = [], $perPage = null, $limit = null)
+    {
+        $qb = $this->createQueryBuilder('q');
+
+        $qb->where('q.isExecutable = 1')
+            ->andWhere('q.executionCount < :executionThreshold')
+            ->setParameter('executionThreshold', OperationsQueue::MAX_QUEUE_THRESHOLD)
+        ;
+
+
+        return $qb->getQuery()->getResult();
+    }
 } 
