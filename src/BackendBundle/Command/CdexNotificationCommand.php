3242599195159cc48e4af9f7007624a76dbcbc83
krossroad <krossroad07@gmail.com>
2015-04-24 12:45:15 +0545

    Add login for cdex:noifiy command

--- src/BackendBundle/Command/CdexNotificationCommand.php
+++ src/BackendBundle/Command/CdexNotificationCommand.php
@@ -1,14 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rikesh
- * Date: 4/24/15
- * Time: 11:02 AM
- */
 
 namespace BackendBundle\Command;
 
 
-class CdexNotificationCommand {
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Class CdexNotificationCommand
+ * @package BackendBundle\Command
+ */
+class CdexNotificationCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('cdex:notify')
+            ->setDescription('Notify the status of transaction.')
+            ->addOption(
+                'operation',
+                null,
+                InputOption::VALUE_REQUIRED                                                                                                                             ,
+                "Notify 'queued', 'failed' or 'both'.",
+                'both'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $operation = $input->getOption('operation');
+        $cdexNotifier = $this->getContainer()->get('transaction.notifier');
+
+        switch ($operation) {
+            case 'queued':
+            case 'failed':
+            case 'both':
+                $count = $cdexNotifier->notify($operation);
+                $output->writeln('<info>'. $count .' Transactions notified successfully.</info>');
+                break;
+
+            default:
+                throw new \Exception('Fatal Error :: Invalid Operation requested!!');
+        }
+    }
 } 
