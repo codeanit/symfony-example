90637a76e3c21b0d5ee53133a2fa6b734fb5fa65
krossroad <krossroad07@gmail.com>
2015-04-24 12:45:55 +0545

    Add repository for notification-request

--- src/BackendBundle/Entity/Repository/NotificationRequestRepository.php
+++ src/BackendBundle/Entity/Repository/NotificationRequestRepository.php
@@ -1,14 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rikesh
- * Date: 4/24/15
- * Time: 12:06 PM
- */
 
 namespace BackendBundle\Entity\Repository;
 
+use BackendBundle\Entity\NotificationRequest;
+use Doctrine\ORM\EntityRepository;
 
-class NotificationRequestRepository {
+/**
+ * Class NotificationRequestRepository
+ * @package BackendBundle\Entity\Repository
+ */
+class NotificationRequestRepository extends EntityRepository
+{
+    /**
+     * @param array $filters
+     * @param array $orderBy
+     * @return array
+     */
+    public function getNotificationRequests($filters = [], $orderBy = [])
+    {
+        $qb = $this->createQueryBuilder('n');
+        $validNotiStat = [];
+        $filterType = (isset($filters['type'])) ? $filters['type'] : 'both';
 
-} 
+        if ($filterType == 'failed') {
+            $validNotiStat[] = NotificationRequest::STATUS_FAILED;
+
+        } elseif ('queued' == $filterType) {
+            $validNotiStat[] = NotificationRequest::STATUS_QUEUED;
+
+        } else {
+            $validNotiStat = [
+                NotificationRequest::STATUS_QUEUED,
+                NotificationRequest::STATUS_FAILED,
+            ];
+        }
+
+        $qb->where('n.notificationStatus IN (:validNotiStats)')
+            ->setParameter('validNotiStats', $validNotiStat)
+        ;
+
+        return $qb->getQuery()->getResult();
+    }
+}
